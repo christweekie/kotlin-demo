9b3238d1640484c6f83b70183fdece3c18b75295
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-10-27 11:31:37 +0100

    remove Bean

--- src/main/kotlin/org/example/_03_functions/funcs.kt
+++ src/main/kotlin/org/example/_03_functions/funcs.kt
@@ -1,21 +1,21 @@
 package org.example._03_functions
 
 class StringUtil {
-  fun getFirstWord(s: String, separator: String): String {
+  fun getSecondWord(s: String, separator: String): String {
     val index = s.indexOf(separator)
     if (index < 0) {
       return s
     }
     else {
-      return s.substring(index)
+      return s.substring(index+1)
     }
   }
 
-  fun getFirstWord(s: String): String {
-    return getFirstWord(s, " ")
+  fun getSecondWord(s: String): String {
+    return getSecondWord(s, " ")
   }
 }
 
 fun main(args: Array<String>) {
-  println(StringUtil().getFirstWord("Jane Doe", " "))
+  println(StringUtil().getSecondWord("Jane Doe", " "))
 }
